669ea174ce324f9a2b0ae9b90bf8638e9df4cc50
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-09-20 17:12:51 +0300

    done

--- tests/test_pythonic.py
+++ tests/test_pythonic.py
@@ -1,5 +1,13 @@
 from pythonic import __version__
+from pythonic.pythonic import *
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_factors_1():
+    actual = factors_one(200)
+    expected = [1, 2, 4, 5, 8, 10, 20, 25, 40, 50,100,200]
+    assert actual == expected
+
+
